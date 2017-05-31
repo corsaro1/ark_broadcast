649757e1f1d9ddc57f8cfad4f1efa44ab97432f8
corsaro1 <ecidshsh@gmail.com>
2017-06-01 01:09:46 +0200

    Update arktx.js

--- arktx.js
+++ arktx.js
@@ -1,4 +1,5 @@
 var ark = require('arkjs');
 var amount      = 1 * Math.pow(10, 8); // 100000000000
 var transaction = ark.transaction.createTransaction("AbEtS8VA4LA1wgJWf4wA3JKyZrfvimp4Ht", amount, null, "firstsecret", "secondsecret");
+// if your address has not a secondSecret, replace "secondsecret" with null
 console.log("%j", transaction);
